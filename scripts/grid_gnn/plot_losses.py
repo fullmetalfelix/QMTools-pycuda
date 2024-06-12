108d9c29fd8c3b549a4616959f8639d8810790f0
NikoOinonen <nikooin@gmail.com>
2024-06-12 12:28:58 +0300

    Updated plot_losses.py.

--- scripts/grid_gnn/plot_losses.py
+++ scripts/grid_gnn/plot_losses.py
@@ -8,28 +8,34 @@ if __name__ == "__main__":
     data_train = np.loadtxt(f"loss_log_train.csv", delimiter=",")
     data_val = np.loadtxt(f"loss_log_val.csv", delimiter=",")
 
+    loss_labels = ["Total loss", "MSE loss", "Gradient MSE loss"]
+
     n_batch_train = data_train[:, 0]
-    loss_train = data_train[:, 1]
+    loss_train = data_train[:, 1:]
 
-    n_batch_val = data_val[:, 0]    
-    n_epoch_val = data_val[:, 1]    
-    loss_val = data_val[:, 2]    
+    n_batch_val = data_val[:, 0]
+    n_epoch_val = data_val[:, 1]
+    loss_val = data_val[:, 2:]
 
-    fig, ax1 = plt.subplots(1, 1, figsize=(7, 5))
-    ax1.semilogy(n_batch_train, loss_train)
-    ax1.semilogy(n_batch_val, loss_val)
-    ax1.set_xlabel("Batch")
-    ax1.set_ylabel("Loss")
-    ax1.legend(["Train", "Validation"])
+    assert loss_train.shape[1] == loss_train.shape[1]
+    n_losses = loss_train.shape[1]
 
-    batches_per_epoch = (n_batch_val[-1] - n_batch_val[0]) / (n_epoch_val[-1] - n_epoch_val[0])
-    n_epoch_min = ax1.get_xlim()[0] / batches_per_epoch
-    n_epoch_max = ax1.get_xlim()[1] / batches_per_epoch
+    fig, axes = plt.subplots(1, n_losses, figsize=(2 + 6 * n_losses, 5))
+    for i in range(n_losses):
+        axes[i].semilogy(n_batch_train, loss_train[:, i])
+        axes[i].semilogy(n_batch_val, loss_val[:, i])
+        axes[i].set_xlabel("Batch")
+        axes[i].set_ylabel(loss_labels[i])
+        axes[i].legend(["Train", "Validation"])
 
-    ax_top = plt.twiny(ax=ax1)
-    ax_top.set_xlim(n_epoch_min, n_epoch_max)
-    ax_top.set_xlabel("Epoch")
-    ax_top.xaxis.set_major_locator(MaxNLocator(integer=True))
+        batches_per_epoch = (n_batch_val[-1] - n_batch_val[0]) / (n_epoch_val[-1] - n_epoch_val[0])
+        n_epoch_min = axes[i].get_xlim()[0] / batches_per_epoch
+        n_epoch_max = axes[i].get_xlim()[1] / batches_per_epoch
+
+        ax_top = plt.twiny(ax=axes[i])
+        ax_top.set_xlim(n_epoch_min, n_epoch_max)
+        ax_top.set_xlabel("Epoch")
+        ax_top.xaxis.set_major_locator(MaxNLocator(integer=True))
 
     plt.savefig(f"loss.png")
     plt.show()
